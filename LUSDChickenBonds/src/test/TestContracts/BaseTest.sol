606dd7bebfee9593f9da4cf4e839414ed0a5a7cd
ßingen <bingen@eguzkitza.com>
2022-04-21 22:44:26 +0200

    Add `assertApproximatelyEqual` with reason

--- LUSDChickenBonds/src/test/TestContracts/BaseTest.sol
+++ LUSDChickenBonds/src/test/TestContracts/BaseTest.sol
@@ -62,8 +62,12 @@ contract BaseTest is DSTest, stdCheats {
     }
 
     function assertApproximatelyEqual(uint256 _x, uint256 _y, uint256 _margin) public {
+        assertApproximatelyEqual(_x, _y, _margin, "");
+    }
+
+    function assertApproximatelyEqual(uint256 _x, uint256 _y, uint256 _margin, string memory _reason) public {
         uint256 diff = abs(_x, _y);
-        assertLe(diff, _margin);
+        assertLe(diff, _margin, _reason);
     }
 
     function assertNotApproximatelyEqual(uint256 _x, uint256 _y, uint256 _margin) public {
