6ed389defc4ecdffc9d0e46a1c90a11312e193ed
Edward Mulraney <edmulraney@gmail.com>
2022-06-09 01:49:46 +0100

    Add support for consuming ChickenBonds in Liquity dev repo

---
+++ scripts/publish-local.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+# Change directory to this location so we can run this script from anywhere
+cd "$(dirname "$0")"
+
+echo -e "Publishing dist to local store...\n"
+
+# Generate dist output to publish
+yarn prepare-compilations
+
+# Publish from the dist directory so that consumers don't have to import with "/dist" in the path
+cp ../package.json ../dist
+yalc push --changed ../dist
+
+echo -e "Finished publishing dist to local store.\n"
---
+++ scripts/watch-publish.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Change directory to this location so we can run this script from anywhere
+cd "$(dirname "$0")"
+
+echo -e "Watching LUSD / LQTY compilations...\n"
+
+nodemon --delay 1 -e js,json --watch ../lusd/dist/ --on-change-only --exec 'yarn publish:local'
+
