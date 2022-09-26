acebd4c29dba53db99d8cc3f49196cb6126f2eea
Daniel Simon <daniel.attila.simon@gmail.com>
2022-09-26 13:57:39 +0700

    feat: add permit functionality to ERC20Faucet

--- LUSDChickenBonds/src/ExternalContracts/Testnet/ERC20Faucet.sol
+++ LUSDChickenBonds/src/ExternalContracts/Testnet/ERC20Faucet.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.10;
 
-import "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-contracts/contracts/token/ERC20/extensions/draft-ERC20Permit.sol";
 import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 import { IMinter } from "./Harvester.sol";
 
-contract ERC20Faucet is IMinter, ERC20, Ownable {
+contract ERC20Faucet is IMinter, ERC20Permit, Ownable {
     uint256 public immutable tapAmount;
     uint256 public immutable tapPeriod;
 
@@ -14,6 +14,7 @@ contract ERC20Faucet is IMinter, ERC20, Ownable {
 
     constructor(string memory _name, string memory _symbol, uint256 _tapAmount, uint256 _tapPeriod)
         ERC20(_name, _symbol)
+        ERC20Permit(_name)
     {
         tapAmount = _tapAmount;
         tapPeriod = _tapPeriod;
