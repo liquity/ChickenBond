cf3cf07dea52e4858e797bae77e3cad08182b78a
ßingen <bingen@eguzkitza.com>
2022-09-28 00:12:29 +0200

    chore: Add mainnet deployment script

---
+++ LUSDChickenBonds/etc/mainnet-dry-run-tests.md
@@ -0,0 +1,32 @@
+# Deployment addresses
+
+```
+{
+    "BLUSD_AMM_ADDRESS": "0xec5ffef96c3EdEdE587DB2efA3ab4Deec414cE8F",
+    "BLUSD_AMM_STAKING_ADDRESS": "0x79DbB869e4f00821927785D21315219Edb016082",
+    "BLUSD_TOKEN_ADDRESS": "0x76F7774139bf0097d2882C41AF5A37717e3641A7",
+    "BOND_NFT_ADDRESS": "0xf80678718187a9e29E63A1f5Af61369ecc8a8a0C",
+    "BOND_NFT_INITIAL_ARTWORK_ADDRESS": "0xfDF1468A6b04307927D83dEf4106dd64839b353b",
+    "CHICKEN_BOND_MANAGER_ADDRESS": "0x89058630b53228aDCC77690586cFb3C74F08803C"
+}
+```
+
+# Tests
+
+[Create bond 1](https://etherscan.io/tx/0xecdbeba69e4bfc278b41bccaa7759e38e613dcd31a17e2b8b50af0e420cc5ec2)
+
+[Failed chicken In during bootstrap period](https://etherscan.io/tx/0x73876384ee74ab706db31df4fe65608836a2f5554e94ff02923f21298e4d103c)
+
+[Create bond (with permit) 2](https://etherscan.io/tx/0x39a3b741f9b33d0d1b5b48c026db0cf5888d2a834b21b97a2aedb88faa6bcb5e)
+
+[Chicken out bond id 2 (still bootstrap period)](https://etherscan.io/tx/0x3363bcea561c580199d6a3733c7b52a72ba04c702e12b2cc18cc76758be0e762)
+
+[Failed chicken In during bootstrap period, 1h after launch, but not after bond creation](https://etherscan.io/tx/0x6a09034133f9438028082289f902eeb0a58c4a3fd899e8a7e75dc65853ad5df9)
+
+[Chicken In bond id 1](https://etherscan.io/tx/0x3cc1a155b0ad78a5af5951417827f6ca13fdbe9dab6633876dd964756bafd871)
+
+[Cannot redeem below min supply](https://etherscan.io/tx/0x866924c65d80a7055f810519984e6a62c86b32fd7f08f45ad0bbd77ade68b034)
+
+[Failed redemption during bootstrap period](https://etherscan.io/tx/0x65be3b5aac624613a5d0651118fae7d2bc0d02bf7d357c429ba04e82ada83ab8)
+
+[Redemption](https://etherscan.io/tx/0x078895c1863a458f381243daae10f4709a874f5862a5eb99a23698907a3769dd)
---
+++ LUSDChickenBonds/scripts/deploy-mainnet.sh
@@ -0,0 +1,332 @@
+#!/bin/bash
+set -e
+
+# Change directory to this file so we can run this script from anywhere
+cd "$(dirname "$0")"
+
+# Colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+RESET_COLOR='\033[0m'
+
+# Script arguments - can also be set as env variables (arguments take precedence over env)
+#  - 1) Ethereum RPC URL
+#  - 2) Deployer private key
+ETH_RPC_URL=${ETH_RPC_URL:-"http://localhost:8545/"}
+DEPLOYER_PRIVATE_KEY=${DEPLOYER_PRIVATE_KEY:-"4d5db4107d237df6a3d58ee5f70ae63d73d7658d4026f2eefd2f204c81682cb7"}
+PRIORITY_GAS_PRICE=${PRIORITY_GAS_PRICE:-""}
+ETHERSCAN_API_KEY=${ETHERSCAN_API_KEY:-""}
+CONFIG_FILE="config/mainnet-dry-run.sh"
+OUTPUT_FILE="../addresses/mainnet-dry-run.json"
+
+usage() {
+    echo "Usage: $0 [ -r ETH_RPC_URL ] [ -k DEPLOYER_PRIVATE_KEY ] [ -e ETHERSCAN_API_KEY ] [ -g PRIORITY_GAS_PRICE ] [ -c CONFIG_FILE ] [ -o OUTPUT_FILE ]" 1>&2
+}
+exit_abnormal() {
+    usage
+    exit 1
+}
+while getopts ":r:k:e:g:c:o:" options; do
+    case "${options}" in
+        r)
+            ETH_RPC_URL=${OPTARG}
+            ;;
+        k)
+            DEPLOYER_PRIVATE_KEY=${OPTARG}
+            ;;
+        e)
+            ETHERSCAN_API_KEY=${OPTARG}
+            ;;
+        g)
+            PRIORITY_GAS_PRICE=${OPTARG}
+            ;;
+        c)
+            CONFIG_FILE=${OPTARG}
+            ;;
+        o)
+            OUTPUT_FILE=${OPTARG}
+            ;;
+        :)
+            echo "Error: -${OPTARG} requires an argument."
+            exit_abnormal
+            ;;
+        *)
+            exit_abnormal
+            ;;
+    esac
+done
+
+DEPLOYER_ADDRESS=$(cast wallet address --private-key $DEPLOYER_PRIVATE_KEY | cut -d" " -f2)
+
+echo ETH_RPC_URL: $ETH_RPC_URL
+#echo DEPLOYER_PRIVATE_KEY: $DEPLOYER_PRIVATE_KEY
+echo DEPLOYER_ADDRESS: $DEPLOYER_ADDRESS
+echo ETHERSCAN_API_KEY: $ETHERSCAN_API_KEY
+echo PRIORITY_GAS_PRICE: $PRIORITY_GAS_PRICE
+echo CONFIG_FILE: $CONFIG_FILE
+echo OUTPUT_FILE: $OUTPUT_FILE
+echo ""
+
+# Load addresses and constructor arguments
+source $CONFIG_FILE
+
+# Make sure RPC URL is up
+cast client --rpc-url $ETH_RPC_URL > /dev/null || {
+  echo -e "\n${RED}ETH_RPC_URL $ETH_RPC_URL isn't up and running.\n"
+  exit 1
+}
+
+# Make sure B.AMM address is set
+[[ -z $MAINNET_BPROTOCOL_LUSD_BAMM_ADDRESS ]] && {
+    echo -e "\n${RED}Missing B.AMM address!\n"
+    exit 1
+}
+
+
+DEPLOYMENT_ADDRESSES=''
+
+# --- Helper functions ---
+
+# Arguments:
+# 1: contract file name
+# 2: constructor params
+# 3: Function to call for checking
+# 4: json key for output addresses
+# 5: contracts subpath
+deploy_contract() {
+    echo "Deploying $1..."
+
+    if [[ -z $2 ]]; then
+        # TODO make it work for non zero args:
+        # ${2:+"--constructor-args $2"}
+        RESULT=$(
+            forge create ./src/$5$1.sol:$1 \
+                  --private-key $DEPLOYER_PRIVATE_KEY \
+                  --rpc-url $ETH_RPC_URL \
+                  ${PRIORITY_GAS_PRICE:+--priority-gas-price $PRIORITY_GAS_PRICE} \
+                  ${ETHERSCAN_API_KEY:+--etherscan-api-key $ETHERSCAN_API_KEY --verify}
+              )
+    else
+        RESULT=$(
+            forge create ./src/$5$1.sol:$1 \
+                  --private-key $DEPLOYER_PRIVATE_KEY \
+                  --rpc-url $ETH_RPC_URL \
+                  ${PRIORITY_GAS_PRICE:+--priority-gas-price $PRIORITY_GAS_PRICE} \
+                  ${ETHERSCAN_API_KEY:+--etherscan-api-key $ETHERSCAN_API_KEY --verify} \
+                  --constructor-args $2
+              )
+    fi
+
+    DEPLOYED_ADDRESS=$(echo $RESULT | sed -nr 's/.* Deployed to: (0x\w+) .*$/\1/p')
+    TX_HASH=$(echo $RESULT | sed -nr 's/.* Transaction hash: (0x\w+)$/\1/p')
+
+    [[ -z $DEPLOYED_ADDRESS ]] && { echo -e "\n${RED}Failed to deploy $1 contract.";  exit 1; }
+
+    RECEIPT=$(cast receipt $TX_HASH)
+    GAS_USED=$(echo $RECEIPT | sed -nr 's/.* gasUsed ([0-9]+) .*$/\1/p')
+
+    # Check deployment
+    cast call $DEPLOYED_ADDRESS $3 --rpc-url $ETH_RPC_URL > /dev/null || {
+        echo -e "\n${RED}Failed to deploy $1 contract."
+        exit 1
+    }
+
+    DEPLOYMENT_ADDRESSES="$DEPLOYMENT_ADDRESSES \"$4\": \"$DEPLOYED_ADDRESS\","
+
+    echo -e "Deployed to: $DEPLOYED_ADDRESS"
+    echo -e "Tx hash: $TX_HASH"
+    echo -e "Gas used: $GAS_USED"
+    echo ""
+}
+
+# Arguments:
+# 1: contract factory address
+# 2: function signature
+# 3: function arguments
+# 4: json key for output addresses
+# 5: contract description (for console logs)
+deploy_from_factory() {
+    echo "Creating $5..."
+    DEPLOYED_ADDRESS=$(
+        cast call $1 $2 $3 \
+             --rpc-url $ETH_RPC_URL \
+             --private-key $DEPLOYER_PRIVATE_KEY \
+                    )
+
+    [[ -z $DEPLOYED_ADDRESS ]] && { echo -e "\n${RED}Failed to deploy $5.";  exit 1; }
+
+    RESULT=$(cast send $1 $2 $3 \
+         --rpc-url $ETH_RPC_URL \
+         --private-key $DEPLOYER_PRIVATE_KEY \
+         ${PRIORITY_GAS_PRICE:+--priority-gas-price $PRIORITY_GAS_PRICE})
+
+    GAS_USED=$(echo $RESULT | sed -nr 's/.* gasUsed ([0-9]+) .*$/\1/p')
+    STATUS=$(echo $RESULT | sed -nr 's/.* status ([0-9]+) .*$/\1/p')
+    TX_HASH=$(echo $RESULT | sed -nr 's/.* transactionHash (0x\w+) .*$/\1/p')
+    [[ $STATUS == 1 ]] || { echo -e "\n${RED}Failed to deploy $5. Status: $STATUS";  exit 1; }
+    [[ -z $TX_HASH ]] && { echo -e "\n${RED}Failed to deploy $5.";  exit 1; }
+
+    DEPLOYMENT_ADDRESSES="$DEPLOYMENT_ADDRESSES \"$4\": \"$DEPLOYED_ADDRESS\","
+
+    echo -e "Deployed to: $DEPLOYED_ADDRESS"
+    echo -e "Tx hash: $TX_HASH"
+    echo -e "Gas used: $GAS_USED"
+    echo ""
+}
+
+# 1: target contract
+# 2: function signature
+# 3: function arguments
+cast_send_wrapper() {
+    RESULT=$(cast send $1 $2 $3 \
+         --rpc-url $ETH_RPC_URL \
+         --private-key $DEPLOYER_PRIVATE_KEY \
+         ${PRIORITY_GAS_PRICE:+--priority-gas-price $PRIORITY_GAS_PRICE})
+
+    GAS_USED=$(echo $RESULT | sed -nr 's/.* gasUsed ([0-9]+) .*$/\1/p')
+    STATUS=$(echo $RESULT | sed -nr 's/.* status ([0-9]+) .*$/\1/p')
+    TX_HASH=$(echo $RESULT | sed -nr 's/.* transactionHash (0x\w+) .*$/\1/p')
+    [[ $STATUS == 1 ]] || { echo -e "\n${RED}Failed to deploy $5. Status: $STATUS";  exit 1; }
+    [[ -z $TX_HASH ]] && { echo -e "\n${RED}Failed to deploy $5.";  exit 1; }
+
+    echo -e "Tx hash: $TX_HASH"
+    echo -e "Gas used: $GAS_USED"
+    echo ""
+}
+
+# --- Deployments ---
+
+# Deploy BLUSDToken contract
+constructor_args="$BLUSD_NAME $BLUSD_SYMBOL"
+deploy_contract "BLUSDToken" "$constructor_args" "owner()(address)" "BLUSD_TOKEN_ADDRESS"
+
+BLUSD_TOKEN_ADDRESS=$DEPLOYED_ADDRESS
+
+# Deploy Egg NFT artwork
+
+# TODO!
+
+#deploy_contract "GenerativeEggArtwork" "" "owner()(address)" "BOND_NFT_INITIAL_ARTWORK_ADDRESS" "NFTArtwork/"
+#BOND_NFT_INITIAL_ARTWORK_ADDRESS=$DEPLOYED_ADDRESS
+
+RESULT=$(forge create ./src/NFTArtwork/GenerativeEggArtwork.sol:GenerativeEggArtwork --private-key $DEPLOYER_PRIVATE_KEY --rpc-url $ETH_RPC_URL ${PRIORITY_GAS_PRICE:+--priority-gas-price $PRIORITY_GAS_PRICE} ${ETHERSCAN_API_KEY:+--etherscan-api-key $ETHERSCAN_API_KEY --verify})
+BOND_NFT_INITIAL_ARTWORK_ADDRESS=$(echo $RESULT | sed -nr 's/.* Deployed to: (0x\w+) .*$/\1/p')
+DEPLOYMENT_ADDRESSES="$DEPLOYMENT_ADDRESSES \"BOND_NFT_INITIAL_ARTWORK_ADDRESS\": \"$BOND_NFT_INITIAL_ARTWORK_ADDRESS\","
+TX_HASH=$(echo $RESULT | sed -nr 's/.* Transaction hash: (0x\w+)$/\1/p')
+RECEIPT=$(cast receipt $TX_HASH)
+GAS_USED=$(echo $RECEIPT | sed -nr 's/.* gasUsed ([0-9]+) .*$/\1/p')
+echo -e Artwork address: $BOND_NFT_INITIAL_ARTWORK_ADDRESS
+echo -e "Tx hash: $TX_HASH"
+echo -e "Gas used: $GAS_USED"
+echo ""
+
+# Deploy BondNFT contract
+LIQUITY_DATA_ADDRESSES="(\
+$MAINNET_LIQUITY_TROVE_MANAGER_ADDRESS,$MAINNET_LQTY_TOKEN_ADDRESS,$MAINNET_LIQUITY_STAKING_ADDRESS,\
+$MAINNET_PICKLE_LQTY_JAR_ADDRESS,$MAINNET_PICKLE_LQTY_FARM_ADDRESS,\
+$MAINNET_CURVE_GAUGE_CONTROLLER,$MAINNET_CURVE_GAUGE_LUSD_3CRV,$MAINNET_CURVE_GAUGE_LUSD_FRAX)"
+constructor_args="$BOND_NFT_NAME $BOND_NFT_SYMBOL $BOND_NFT_INITIAL_ARTWORK_ADDRESS $BOND_NFT_LOCKOUT_PERIOD $LIQUITY_DATA_ADDRESSES"
+deploy_contract "BondNFT" "$constructor_args" "owner()(address)" "BOND_NFT_ADDRESS"
+
+BOND_NFT_ADDRESS=$DEPLOYED_ADDRESS
+
+# Create bLUSD AMM pool
+deployment_arguments="$CURVE_V2_NAME $CURVE_V2_SYMBOL [$BLUSD_TOKEN_ADDRESS,$MAINNET_LUSD_TOKEN_ADDRESS] \
+$CURVE_V2_A $CURVE_V2_GAMMA $CURVE_V2_MID_FEE $CURVE_V2_OUT_FEE $CURVE_V2_ALLOWED_EXTRA_PROFIT $CURVE_V2_FEE_GAMMA \
+$CURVE_V2_ADJUSTMENT_STEP $CURVE_V2_ADMIN_FEE $CURVE_V2_MA_HALF_TIME $CURVE_V2_INITIAL_PRICE"
+deploy_from_factory \
+    $MAINNET_CURVE_V2_FACTORY_ADDRESS \
+    "deploy_pool(string,string,address[2],uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256,uint256)(address)" \
+    "$deployment_arguments" \
+    "BLUSD_AMM_ADDRESS" \
+    "bLUSD/LUSD Curve AMM pool"
+
+BLUSD_AMM_ADDRESS=$DEPLOYED_ADDRESS
+
+# Create bLUSD AMM staking reward contract
+deployment_arguments="$BLUSD_AMM_ADDRESS $DEPLOYER_ADDRESS"
+deploy_from_factory \
+    $MAINNET_CURVE_V2_GAUGE_MANAGER_PROXY_ADDRESS \
+    "deploy_gauge(address,address)(address)" \
+    "$deployment_arguments" \
+    "BLUSD_AMM_STAKING_ADDRESS" \
+    "bLUSD/LUSD staking reward contract (Curve gauge)"
+
+BLUSD_AMM_STAKING_ADDRESS=$DEPLOYED_ADDRESS
+
+# Deploy ChickenBondManager contract
+EXTERNAL_ADDRESSES="(\
+$BOND_NFT_ADDRESS,\
+$MAINNET_LUSD_TOKEN_ADDRESS,\
+$MAINNET_CURVE_POOL_ADDRESS,\
+$MAINNET_CURVE_BASEPOOL_ADDRESS,\
+$MAINNET_BPROTOCOL_LUSD_BAMM_ADDRESS,\
+$MAINNET_YEARN_CURVE_VAULT_ADDRESS,\
+$MAINNET_YEARN_REGISTRY_ADDRESS,\
+$MAINNET_YEARN_GOVERNANCE_ADDRESS,\
+$BLUSD_TOKEN_ADDRESS,\
+$BLUSD_AMM_STAKING_ADDRESS)"
+
+PARAMS="(\
+$TARGET_AVERAGE_AGE_SECONDS,\
+$INITIAL_ACCRUAL_PARAMETER,\
+$MINIMUM_ACCRUAL_PARAMETER,\
+$ACCRUAL_ADJUSTMENT_RATE,\
+$ACCRUAL_ADJUSTMENT_PERIOD_SECONDS,\
+$CHICKEN_IN_AMM_FEE,\
+$CURVE_DEPOSIT_WITHDRAW_DYDX_THRESHOLD,\
+$CURVE_DEPOSIT_WITHDRAW_DYDX_THRESHOLD,\
+$BOOTSTRAP_PERIOD_CHICKEN_IN,\
+$BOOTSTRAP_PERIOD_REDEEM,\
+$BOOTSTRAP_PERIOD_SHIFT,\
+$SHIFTER_DELAY,\
+$SHIFTER_WINDOW,\
+$MIN_BLUSD_SUPPLY,\
+$MIN_BOND_AMOUNT,\
+$NFT_RANDOMNESS_DIVISOR,\
+$REDEMPTION_FEE_BETA,\
+$REDEMPTION_FEE_MINUTE_DECAY_FACTOR)"
+
+constructor_args="$EXTERNAL_ADDRESSES $PARAMS"
+deploy_contract "ChickenBondManager" "$constructor_args" "getPendingLUSD()(uint256)" "CHICKEN_BOND_MANAGER_ADDRESS"
+
+CHICKEN_BOND_MANAGER_ADDRESS=$DEPLOYED_ADDRESS
+
+# Connect ChickenBond contracts
+echo "Connecting BondNFT to ChickenBondManager..."
+cast_send_wrapper $BOND_NFT_ADDRESS "setAddresses(address)" "$CHICKEN_BOND_MANAGER_ADDRESS"
+
+echo "Connecting BLUSDToken to ChickenBondManager..."
+cast_send_wrapper $BLUSD_TOKEN_ADDRESS "setAddresses(address)" "$CHICKEN_BOND_MANAGER_ADDRESS"
+
+echo "Adding LUSD reward token for bLUSD/LUSD LPs, and set ChickenBondManager as distributor in Curve gauge..."
+cast_send_wrapper \
+    $MAINNET_CURVE_V2_GAUGE_MANAGER_PROXY_ADDRESS \
+    "add_reward(address,address,address)" \
+    "$BLUSD_AMM_STAKING_ADDRESS $MAINNET_LUSD_TOKEN_ADDRESS $CHICKEN_BOND_MANAGER_ADDRESS"
+
+echo "Connecting ChickenBondManager to BAMM..."
+cast_send_wrapper $MAINNET_BPROTOCOL_LUSD_BAMM_ADDRESS "setChicken(address)" "$CHICKEN_BOND_MANAGER_ADDRESS"
+
+echo -e "${GREEN}Finished.\n"
+echo -e "${RESET_COLOR}"
+
+# Finish and save deployment addresses json
+DEPLOYMENT_ADDRESSES="{${DEPLOYMENT_ADDRESSES::-1}}"
+#echo -e "${DEPLOYMENT_ADDRESSES}\n"
+
+PYTHON_CMD="python"
+if ! command -v $PYTHON_CMD &> /dev/null
+then
+  PYTHON_CMD="python3"
+  if ! command -v $PYTHON_CMD &> /dev/null
+  then
+    echo "Couldn't find python or python3, do you have it installed under a different alias?"
+    exit
+  fi
+fi
+
+echo $DEPLOYMENT_ADDRESSES | $PYTHON_CMD -m json.tool > $OUTPUT_FILE
+
+cat $OUTPUT_FILE
+
